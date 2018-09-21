d0cc57f5f237818d0f19fbd4ab872864fdcadca4
agavrilenko <trash@inbox.ru>
2018-09-20 20:17:38 -0400

    Detect cycles in list

---
+++ hackerrank/src/test/java/org/my/hrank/data_structures/linked_lists/cycle_detection/CycleDetectionSolutionTest.java
@@ -0,0 +1,13 @@
+package org.my.hrank.data_structures.linked_lists.cycle_detection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CycleDetectionSolutionTest {
+    @Test
+    public void testCycles(){
+
+    }
+
+}

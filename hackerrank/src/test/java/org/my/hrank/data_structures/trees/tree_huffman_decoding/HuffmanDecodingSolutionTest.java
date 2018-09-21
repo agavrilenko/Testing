8ddd2d5fe5e02fb390e4843b8faba5988c9f692f
agavrilenko <trash@inbox.ru>
2018-09-21 19:12:06 -0400

    Huffman decoding

---
+++ hackerrank/src/test/java/org/my/hrank/data_structures/trees/tree_huffman_decoding/HuffmanDecodingSolutionTest.java
@@ -0,0 +1,15 @@
+package org.my.hrank.data_structures.trees.tree_huffman_decoding;
+
+import org.my.hrank.AbstractTest;
+
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class HuffmanDecodingSolutionTest extends AbstractTest {
+
+    @Override
+    public void testAndAssert(Scanner inScanner, Scanner outScanner, String absolutePath) {
+
+    }
+}

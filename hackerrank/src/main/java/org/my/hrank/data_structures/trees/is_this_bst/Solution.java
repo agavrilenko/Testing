6823869e76dd5c13a0c4ab6650aab795ce7cfc5b
agavrilenko <trash@inbox.ru>
2018-09-30 16:16:27 -0400

    Is BST. fails some test cases

--- hackerrank/src/main/java/org/my/hrank/data_structures/trees/is_this_bst/Solution.java
+++ hackerrank/src/main/java/org/my/hrank/data_structures/trees/is_this_bst/Solution.java
@@ -2,7 +2,67 @@ package org.my.hrank.data_structures.trees.is_this_bst;
 
 public class Solution {
     public static boolean checkBST(Node root) {
-        return false;
+
+        if (root == null) {
+            return true;
+        }
+
+        boolean isBst = true;
+        if (root.left != null) {
+            int maxLeft = max(root.left);
+            if (maxLeft == -1) {
+                return false;
+            }
+            isBst = maxLeft < root.data;
+        }
+        if (root.right != null) {
+            int minRight = min(root.right);
+            if (minRight == -1) {
+                return false;
+            }
+            isBst = isBst && minRight > root.data;
+        }
+        return isBst;
+    }
+
+    private static int max(Node root) {
+
+        int max = root.data;
+        if (root.left != null) {
+            int maxLeft = max(root.left);
+            if (maxLeft == -1 || root.data <= maxLeft) {
+                return -1;
+            }
+            max = max > maxLeft ? max : maxLeft;
+        }
+        if (root.right != null) {
+            int minRight = min(root.right);
+            if (minRight == -1 || root.data >= minRight) {
+                return -1;
+            }
+            max = max > minRight ? max : minRight;
+        }
+        return max;
+    }
+
+    static int min(Node root) {
+        int min = root.data;
+        if (root.left != null) {
+            int maxLeft = max(root.left);
+            if (maxLeft == -1 || root.data <= maxLeft) {
+                return -1;
+            }
+            min = min < maxLeft ? min : maxLeft;
+        }
+        if (root.right != null) {
+            int minRight = min(root.right);
+            if (minRight == -1 || root.data >= minRight) {
+                return -1;
+            }
+            min = min < minRight ? min : minRight;
+        }
+        return min;
+
     }
 
     static class Node {
--- hackerrank/src/test/java/org/my/hrank/data_structures/trees/is_this_bst/IsThisBSTSolutionTest.java
+++ hackerrank/src/test/java/org/my/hrank/data_structures/trees/is_this_bst/IsThisBSTSolutionTest.java
@@ -1,5 +1,6 @@
 package org.my.hrank.data_structures.trees.is_this_bst;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,7 +8,7 @@ import static org.my.hrank.data_structures.trees.is_this_bst.Solution.*;
 
 public class IsThisBSTSolutionTest {
     @Test
-    public void testBST_1(){
+    public void testBST_False_1() {
 
         Node root = new Node(3);
         Node left = new Node(5);
@@ -18,7 +19,129 @@ public class IsThisBSTSolutionTest {
         left.right = new Node(4);
         right.left = new Node(6);
 
+        Assert.assertFalse(Solution.checkBST(root));
+    }
 
+    @Test
+    public void testBST_False_2() {
+
+        Node root = new Node(6);
+        Node left = new Node(4);
+        root.left = left;
+        left.left = new Node(2);
+        left.right = new Node(3);
+        Node right = new Node(9);
+        root.right = right;
+        right.left = new Node(8);
+        right.right = new Node(10);
+        right.right.right = new Node(11);
+
+        Assert.assertFalse(Solution.checkBST(root));
+    }
+
+    @Test
+    public void testBST_False_3() {
+
+        Node root = new Node(6);
+        Node left = new Node(4);
+        root.left = left;
+        left.left = new Node(2);
+        left.right = new Node(12);
+        Node right = new Node(9);
+        root.right = right;
+        right.left = new Node(8);
+        right.right = new Node(10);
+        right.right.right = new Node(11);
+
+        Assert.assertFalse(Solution.checkBST(root));
+    }
+
+    @Test
+    public void testBST_False_4() {
+
+        Node root = new Node(6);
+        Node left = new Node(4);
+        root.left = left;
+        left.left = new Node(2);
+        left.right = new Node(3);
+        Node right = new Node(9);
+        root.right = right;
+        right.left = new Node(9);
+        right.right = new Node(10);
+        right.right.right = new Node(11);
+
+        Assert.assertFalse(Solution.checkBST(root));
+    }
+@Test
+    public void testBST_False_5() {
+
+        Node root = new Node(1);
+        Node right = new Node(2);
+        root.right = right;
+        right.left = new Node(2);
+        right.right = new Node(10);
+        right.right.right = new Node(11);
+
+        Assert.assertFalse(Solution.checkBST(root));
+    }
+
+    @Test
+    public void testBST_True_2() {
+
+        Node root = new Node(6);
+        Node left = new Node(4);
+        Node right = new Node(9);
+        root.left = left;
+        left.left = new Node(2);
+        left.right = new Node(5);
+        root.right = right;
+        right.left = new Node(7);
+        right.right = new Node(10);
+        right.right.right = new Node(11);
+
+        Assert.assertTrue(Solution.checkBST(root));
+    }
+
+    @Test
+    public void testBST_True_4() {
+
+        Node root = new Node(6);
+        Node left = new Node(4);
+        root.left = left;
+        left.left = new Node(2);
+        left.right = new Node(5);
+
+        Assert.assertTrue(Solution.checkBST(root));
+    }
+
+    @Test
+    public void testBST_True_3() {
+
+        Node root = new Node(6);
+        Node right = new Node(9);
+        root.right = right;
+        right.left = new Node(7);
+        right.right = new Node(10);
+        right.right.right = new Node(11);
+
+        Assert.assertTrue(Solution.checkBST(root));
+    }
+
+    @Test
+    public void testBST_True_1() {
+
+        Node root = new Node(6);
+        Node left = new Node(4);
+        root.left = left;
+        left.left = new Node(2);
+        left.right = new Node(5);
+        Node right = new Node(9);
+        root.right = right;
+        right.left = new Node(8);
+        right.right = new Node(10);
+        right.right.right = new Node(11);
+
+        Assert.assertTrue(Solution.checkBST(root));
     }
 
 }

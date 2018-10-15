35196b6f16e47eedf5e5014b1e38a16660d2ad75
agavrilenko <trash@inbox.ru>
2018-10-14 21:29:57 -0400

    Find max index product.

---
+++ hackerrank/src/test/java/org/my/hrank/data_structures/advanced/find_max_index_product/FindMaxIndexProductSolutionTest.java
@@ -0,0 +1,32 @@
+package org.my.hrank.data_structures.advanced.find_max_index_product;
+
+import org.hamcrest.core.IsEqual;
+import org.my.hrank.AbstractTest;
+
+import java.util.Scanner;
+
+import static org.my.hrank.data_structures.advanced.find_max_index_product.Solution.solve;
+
+public class FindMaxIndexProductSolutionTest extends AbstractTest {
+
+    @Override
+    public void testAndAssert(Scanner inScanner, Scanner outScanner, String absolutePath) throws Exception {
+        int arrCount = inScanner.nextInt();
+        inScanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[arrCount];
+
+        String[] arrItems = inScanner.nextLine().split(" ");
+        inScanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int arrItr = 0; arrItr < arrCount; arrItr++) {
+            int arrItem = Integer.parseInt(arrItems[arrItr]);
+            arr[arrItr] = arrItem;
+        }
+
+
+        int expected = Integer.parseInt(outScanner.nextLine().trim());
+        int result = solve(arr);
+        collector.checkThat(getMessage(absolutePath), result, IsEqual.equalTo(expected));
+    }
+}
---
+++ hackerrank/src/test/java/org/my/hrank/data_structures/advanced/find_max_index_product/Solution.java
@@ -0,0 +1,43 @@
+package org.my.hrank.data_structures.advanced.find_max_index_product;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    // Complete the solve function below.
+    static int solve(int[] arr) {
+
+        return -1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[arrCount];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int arrItr = 0; arrItr < arrCount; arrItr++) {
+            int arrItem = Integer.parseInt(arrItems[arrItr]);
+            arr[arrItr] = arrItem;
+        }
+
+        int result = solve(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}

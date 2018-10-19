5ea7cf250b3c6f6e08e348e05fec4424a7f49c31
agavrilenko <trash@inbox.ru>
2018-10-18 22:27:45 -0400

    Max index product.

--- hackerrank/src/main/java/org/my/hrank/data_structures/advanced/find_max_index_product/Solution.java
+++ hackerrank/src/main/java/org/my/hrank/data_structures/advanced/find_max_index_product/Solution.java
@@ -10,7 +10,124 @@ public class Solution {
     // Complete the solve function below.
     static int solve(int[] arr) {
 
-        return -1;
+        Node[] nodes = new Node[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            nodes[i] = new Node(arr[i], i);
+        }
+
+        //find max left
+        Node root = nodes[0];
+        for (int i = 1; i < nodes.length; i++) {
+            findMaxLeft(root, nodes[i]);
+            addNodeToTree(root, nodes[i]);
+        }
+        for (Node node : nodes) {
+            node.left = null;
+            node.right = null;
+        }
+        root = nodes[nodes.length - 1];
+        for (int i = nodes.length - 2; i >= 0; i--) {
+            findMaxRight(root, nodes[i]);
+            addNodeToTree(root, nodes[i]);
+        }
+
+        int sum = 0;
+        for (Node node : nodes) {
+            int tmp = 0;
+            int maxRight = node.maxRight;
+            int maxLeft = node.maxLeft;
+            if (maxRight != -1 && maxLeft != -1) {
+                tmp = (maxLeft + 1) * (maxRight + 1);
+            }
+            sum = sum > tmp ? sum : tmp;
+        }
+
+        // if node.value > root - go right
+        // if node.value <= root - go left
+
+        //find max right
+
+
+        return sum;
+    }
+
+    private static void addNodeToTree(Node root, Node node) {
+        if (root.value < node.value) {
+            if (root.right != null) {
+                addNodeToTree(root.right, node);
+            } else {
+                root.right = node;
+            }
+        } else {
+            if (root.left != null) {
+                addNodeToTree(root.left, node);
+            } else {
+                root.left = node;
+            }
+        }
+    }
+
+    private static void findMaxLeft(Node root, Node node) {
+
+        if (node.value > root.value && root.right != null) {
+            //traverse right. Left doest have bigger value
+            findMaxLeft(root.right, node);
+        } else {
+            if (root.value > node.value && root.index > node.maxLeft) {
+                node.maxLeft = root.index;
+            }
+            if (root.left != null && root.left.value >= node.value) {
+                findMaxLeft(root.left, node);
+            }
+            if (root.right != null) {
+                findMaxLeft(root.right, node);
+            }
+        }
+
+    }
+
+    private static void findMaxRight(Node root, Node node) {
+
+        if (node.value > root.value && root.right != null) {
+            //traverse right. Left doest have bigger value
+            findMaxRight(root.right, node);
+        } else {
+            if (root.value > node.value && root.index < node.maxRight) {
+                node.maxRight = root.index;
+            }
+            if (root.left != null && root.left.value >= node.value) {
+                findMaxRight(root.left, node);
+            }
+            if (root.right != null) {
+                findMaxRight(root.right, node);
+            }
+
+        }
+
+    }
+
+    public static class Node {
+
+        private int value;
+        private Node left, right;
+        private int index;
+        private int maxLeft = -1;
+        private int maxRight = Integer.MAX_VALUE;
+
+        public Node(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", index=" + index +
+                    ", maxLeft=" + maxLeft +
+                    ", maxRight=" + maxRight +
+                    '}';
+        }
     }
 
     private static final Scanner scanner = new Scanner(System.in);

788d75fe42acc5eb03ac903a4a4fdfd2d0b36a11
agavrilenko <trash@inbox.ru>
2018-10-21 00:04:02 -0400

    Waiter.Solved.

--- hackerrank/src/main/java/org/my/hrank/data_structures/stacks/waiter/WaiterSolution.java
+++ hackerrank/src/main/java/org/my/hrank/data_structures/stacks/waiter/WaiterSolution.java
@@ -3,6 +3,10 @@ package org.my.hrank.data_structures.stacks.waiter;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WaiterSolution {
@@ -10,9 +14,67 @@ public class WaiterSolution {
      * Complete the waiter function below.
      */
     static int[] waiter(int[] number, int q) {
-        return new int[0];
+
+        List<Integer> primeNumbers = findNthPrimeNumber(q);
+        LinkedList<Integer>[] stacks = (LinkedList<Integer>[]) new LinkedList[q + 1];
+        for (int i = 0; i < stacks.length; i++) {
+            stacks[i] = new LinkedList<>();
+        }
+
+        for (int j = number.length - 1; j >= 0; j--) {
+            int num = number[j];
+            for (int i = 0; i < q; i++) {
+                Integer prime = primeNumbers.get(i);
+                if (num % prime == 0) {
+                    if (i % 2 == 0) {
+                        stacks[i].addFirst(num);
+                    } else {
+                        stacks[i].addLast(num);
+                    }
+                    break;
+                }
+                if (i == q - 1) {
+                    if (q % 2 == 0) {
+                        stacks[q].addLast(num);
+                    } else {
+                        stacks[q].addFirst(num);
+
+                    }
+                }
+            }
+        }
+
+        LinkedList<Integer> consolidated = new LinkedList<>();
+        for (LinkedList list : stacks) {
+            consolidated.addAll(list);
+        }
+        int[] result = new int[consolidated.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = consolidated.removeFirst();
+
+        }
+
+        return result;
 
     }
+
+    public static List<Integer> findNthPrimeNumber(Integer nthNumber) {
+        List<Integer> primeList = new ArrayList<>();
+        primeList.addAll(Arrays.asList(2, 3));
+        Integer initializer = 4;
+        while (primeList.size() < nthNumber) {
+            if (isPrime(initializer, primeList)) {
+                primeList.add(initializer);
+            }
+            initializer++;
+        }
+        return primeList;
+    }
+
+    public static Boolean isPrime(Integer input, List<Integer> primeList) {
+        return !(primeList.parallelStream().anyMatch(i -> input % i == 0));
+    }
+
 
     private static final Scanner scanner = new Scanner(System.in);
 

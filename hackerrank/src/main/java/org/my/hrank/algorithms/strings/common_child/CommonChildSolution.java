0b8dcc365ac433096d62874b3ee9987c38e033a6
agavrilenko <trash@inbox.ru>
2018-10-23 22:01:59 -0400

    Common child. passed.

--- hackerrank/src/main/java/org/my/hrank/algorithms/strings/common_child/CommonChildSolution.java
+++ hackerrank/src/main/java/org/my/hrank/algorithms/strings/common_child/CommonChildSolution.java
@@ -3,14 +3,49 @@ package org.my.hrank.algorithms.strings.common_child;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class CommonChildSolution {
     // Complete the commonChild function below.
     static int commonChild(String s1, String s2) {
 
-        return -1;
+        char[] s1Chars = s1.toCharArray();
+        char[] s2Chars = s2.toCharArray();
+        int[][] matrix = new int[s1Chars.length + 1][s2Chars.length + 1];
 
+        //first column and row are 0. chars will be at i-1, j-1
+        for (int i = 1; i < s1Chars.length + 1; i++) {
+            for (int j = 1; j < s2Chars.length + 1; j++) {
+                if (s1Chars[i - 1] == s2Chars[j - 1]) {
+                    matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                } else {
+                    matrix[i][j] = getMax(matrix[i - 1][j], matrix[i][j - 1]);
+                }
+            }
+        }
+
+
+        return matrix[s1Chars.length][s2Chars.length];
+
+    }
+
+    private static void backtrackAll(int[][] matrix, char[] first, char[] second, int i, int j, LinkedList<String> result) {
+        if (i == 0 || j == 0) {
+            return;
+        }
+        if (first[i] == second[j]) {
+            backtrackAll(matrix, first, second, i - 1, j - 1, result);
+            result.forEach(st -> st = st.concat(String.valueOf(first[i])));
+            return;
+        }
+        if (matrix[i][j - 1] >= matrix[i - 1][j]) {
+        }
+
+    }
+
+    private static int getMax(int max, int current) {
+        return max > current ? max : current;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
--- hackerrank/src/test/java/org/my/hrank/AbstractPackageTest.java
+++ hackerrank/src/test/java/org/my/hrank/AbstractPackageTest.java
@@ -4,7 +4,8 @@ import java.io.File;
 
 public abstract class AbstractPackageTest extends AbstractTest {
     protected File[] getTestFiles() {
-        String inFolder = "src/test/resources/" + this.getClass().getPackage().getName().replace(".", "/") + "/in";
+        String inFolder = "src/test/resources/" + this.getClass().getPackage().getName().replace(".", "/")
+                + "/" + this.getClass().getSimpleName() + "/in";
 
         return new File(inFolder).listFiles();
     }

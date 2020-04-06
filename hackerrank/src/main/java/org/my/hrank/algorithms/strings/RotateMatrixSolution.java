3e67dfbe1e93f4cd1dfb0636968bba72a2389815
gavrilol <trash@inbox.ru>
2020-04-06 00:01:50 -0400

    Rotate matrix with defect

--- hackerrank/src/main/java/org/my/hrank/algorithms/strings/RotateMatrixSolution.java
+++ hackerrank/src/main/java/org/my/hrank/algorithms/strings/RotateMatrixSolution.java
@@ -2,8 +2,32 @@ package org.my.hrank.algorithms.strings;
 
 public class RotateMatrixSolution {
     public int[][] rotate(int[][] matrix) {
-
-
+        int len = matrix.length;
+        for (int i = 0; i <= (len - 1) / 2; i++) {
+            for (int j = i; j < len - i - 1; j++) {
+                rotateFour(len, i, j, matrix);
+            }
+        }
         return matrix;
+    }
+
+    private void rotateFour(int len, int i, int j, int[][] matrix) {
+        int tmp, tmp1, tmp2, tmp3;
+        int l = len - 2 * i;//  length of square
+        if (l == 1) {
+            return;
+        }
+        int delta = 0;
+        if(l==2){
+            delta = 1;
+        }
+        tmp = matrix[i][j];
+        tmp1 = matrix[j + i - delta][i + l - 1];
+        tmp2 = matrix[i + l - 1][i + l - 1 - j + delta];
+        tmp3 = matrix[i + l - 1 - j + delta][i];
+        matrix[i][j] = tmp3;
+        matrix[j + i - delta][i + l - 1] = tmp;
+        matrix[i + l - 1][i + l - 1 - j + delta] = tmp1;
+        matrix[i + l - 1 - j + delta][i] = tmp2;
     }
 }
--- hackerrank/src/test/java/org/my/hrank/algorithms/strings/RotateMatrixSolutionTest.java
+++ hackerrank/src/test/java/org/my/hrank/algorithms/strings/RotateMatrixSolutionTest.java
@@ -39,9 +39,29 @@ public class RotateMatrixSolutionTest {
         };
         out = new int[][]
                 {{15, 13, 2, 5},
-                 {14, 3, 4, 1},
-                 {12, 6, 8, 9},
-                 {16, 7, 10, 11}};
+                        {14, 3, 4, 1},
+                        {12, 6, 8, 9},
+                        {16, 7, 10, 11}};
+        Assert.assertArrayEquals(out, solution.rotate(in));
+
+    }
+
+    @Test
+    public void testSimple5() {
+        in = new int[][]{
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25},
+        };
+        out = new int[][]
+                {{21, 16, 11, 6, 1},
+                        {22, 17, 12, 7, 2},
+                        {23, 18, 13, 8, 3},
+                        {24, 19, 14, 9, 4},
+                        {25, 20, 15, 10, 5}
+                };
         Assert.assertArrayEquals(out, solution.rotate(in));
 
     }

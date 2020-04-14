429e84334d212484ca3df059ad39dacd1ba216a0
agavrilenko <trash@inbox.ru>
2020-04-13 23:49:15 -0400

    Bitwise AND of Numbers Range https://leetcode.com/problems/bitwise-and-of-numbers-range/

--- hackerrank/src/main/java/org/my/hrank/data_structures/arrays/bitwise_and_numbers_range/BitwiseAndOfNumbersRangeSolution.java
+++ hackerrank/src/main/java/org/my/hrank/data_structures/arrays/bitwise_and_numbers_range/BitwiseAndOfNumbersRangeSolution.java
@@ -3,7 +3,29 @@ package org.my.hrank.data_structures.arrays.bitwise_and_numbers_range;
 public class BitwiseAndOfNumbersRangeSolution {
 
     public int rangeBitwiseAnd(int m, int n) {
+        int depth = n - m;
+        int current = m;
+        int lastBit = 0;
+        int pos = 0;
+        int tail = 0;
+        //moving one bit from right to left
+        while (current > 0) {
+            lastBit = current & 1;
+            //calculate tail and check weather it is greater than number of elements in range
+            tail = lastBit * (int) Math.pow(2, pos) + tail;
+            //if tail greater than number of elements it means that major bits will remain same
+            if (Math.pow(2, pos + 1) - tail > depth) {
+                break;
+            }
+            pos++;
+            //bitwise move to the right.
+            current = current >> 1;
+        }
+        current <<= pos;
+        return current & m;
+    }
 
+    public int rangeBitwiseAnd1(int m, int n) {
         int[] bin = new int[32];
         int pow = 0;
         int divided = m;
@@ -47,5 +69,6 @@ public class BitwiseAndOfNumbersRangeSolution {
         }
         return sum;
     }
+
 
 }
